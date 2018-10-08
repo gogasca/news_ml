8e5217f440efa208e2613ca790d69a1479e3cfd4
Gonzalo Gasca Meza <gogasca@google.com>
2018-10-08 15:44:11 -0700

    fixed untracked files

--- services/__init__.py
+++
@@ -1,8 +0,0 @@
-import platform
-import sys
-
-if platform.system() == 'Linux':
-    filepath = '/usr/local/src/news_ml/'
-else:
-    filepath = '/Users/gogasca/Documents/Development/dpe/news/'
-sys.path.append(filepath)
